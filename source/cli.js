57317b5f6bf4b0c562defcd181e7b31b3e83dcb5
Waren Gonzaga <opensource@warengonzaga.com>
2024-02-22 23:22:10 +0800

    📦 new: add xplorescan module

--- source/cli.js
+++ source/cli.js
@@ -2,18 +2,41 @@
 import React from 'react';
 import { render } from 'ink';
 import meow from 'meow';
+import open from 'open';
 import App from './app.js';
 
 const cli = meow(
 	`
 		Usage
 			$ web3ph
+			$ web3ph explore <address> --eth
+			$ web3ph explore <address> --matic
 
 		We're still in development. Please wait for the next update.
 	`,
 	{
 		importMeta: import.meta,
+		flags: {
+			ethereum: {
+				type: 'boolean',
+				alias: 'eth',
+				default: false
+			},
+			polygon: {
+				type: 'boolean',
+				alias: 'matic',
+				default: false
+			},
+		},
 	},
 );
 
 render(<App/>);
+
+if (cli.input[0] === 'explore' && cli.input[1] && cli.flags.ethereum) {
+    open(`https://etherscan.io/address/${cli.input[1]}`);
+} else if (cli.input[0] === 'explore' && cli.input[1] && cli.flags.polygon) {
+	open(`https://polygonscan.com/address/${cli.input[1]}`);
+} else {
+    console.error('Invalid command or address');
+}
